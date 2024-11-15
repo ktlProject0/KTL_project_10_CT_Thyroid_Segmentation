f0df61dd59bd1886f004172e39059c2e4fb216fc
ktlProject0 <ktlproject0@gmail.com>
2024-11-15 18:13:08 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,13 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class DenseBlock3D(nn.Module):
+class DenseBlock2D(nn.Module):
     def __init__(self, in_channels, growth_rate, n_layers, use_batchnorm=True):
-        super(DenseBlock3D, self).__init__()
+        super(DenseBlock2D, self).__init__()
         self.layers = nn.ModuleList()
         for i in range(n_layers):
             layer_in_channels = in_channels + i * growth_rate
-            self.layers.append(Conv3dLayer(layer_in_channels, growth_rate, use_batchnorm))
+            self.layers.append(Conv2dLayer(layer_in_channels, growth_rate, use_batchnorm))
 
     def forward(self, x):
         for layer in self.layers:
@@ -16,37 +15,37 @@ class DenseBlock3D(nn.Module):
             x = torch.cat([x, out], dim=1)
         return x
 
-class Conv3dLayer(nn.Module):
+class Conv2dLayer(nn.Module):
     def __init__(self, in_channels, out_channels, use_batchnorm=True):
-        super(Conv3dLayer, self).__init__()
+        super(Conv2dLayer, self).__init__()
         layers = [
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         ]
         if use_batchnorm:
-            layers.insert(1, nn.BatchNorm3d(out_channels))
+            layers.insert(1, nn.BatchNorm2d(out_channels))
         self.layer = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.layer(x)
 
-class TransitionDown3D(nn.Module):
+class TransitionDown2D(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(TransitionDown3D, self).__init__()
+        super(TransitionDown2D, self).__init__()
         self.transition = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=1),
-            nn.BatchNorm3d(out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
     def forward(self, x):
         return self.transition(x)
 
-class TransitionUp3D(nn.Module):
+class TransitionUp2D(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(TransitionUp3D, self).__init__()
-        self.upconv = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        super(TransitionUp2D, self).__init__()
+        self.upconv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
     def forward(self, x):
         return self.upconv(x)
@@ -54,40 +53,32 @@ class TransitionUp3D(nn.Module):
 class Net(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, init_features=32, growth_rate=16, n_dense_layers=4):
         super(Net, self).__init__()
+        self.init_conv = nn.Conv2d(in_channels, init_features, kernel_size=3, padding=1)
+        self.dense_block1 = DenseBlock2D(init_features, growth_rate, n_dense_layers)
+        self.trans_down1 = TransitionDown2D(init_features + n_dense_layers * growth_rate, init_features * 2)
+        self.dense_block2 = DenseBlock2D(init_features * 2, growth_rate, n_dense_layers)
+        self.trans_down2 = TransitionDown2D(init_features * 2 + n_dense_layers * growth_rate, init_features * 4)
+        self.dense_block3 = DenseBlock2D(init_features * 4, growth_rate, n_dense_layers)
+        self.trans_down3 = TransitionDown2D(init_features * 4 + n_dense_layers * growth_rate, init_features * 8)
 
-        # Encoder
-        self.init_conv = nn.Conv3d(in_channels, init_features, kernel_size=3, padding=1)
+        self.bottleneck = DenseBlock2D(init_features * 8, growth_rate, n_dense_layers)
 
-        self.dense_block1 = DenseBlock3D(init_features, growth_rate, n_dense_layers)
-        self.trans_down1 = TransitionDown3D(init_features + n_dense_layers * growth_rate, init_features * 2)
+        self.trans_up3 = TransitionUp2D(init_features * 8 + n_dense_layers * growth_rate, init_features * 4)
+        self.reduce_conv3 = nn.Conv2d(init_features * 4 + init_features * 4 + n_dense_layers * growth_rate, init_features * 4, kernel_size=1)
+        self.dense_block4 = DenseBlock2D(init_features * 4, growth_rate, n_dense_layers)
 
-        self.dense_block2 = DenseBlock3D(init_features * 2, growth_rate, n_dense_layers)
-        self.trans_down2 = TransitionDown3D(init_features * 2 + n_dense_layers * growth_rate, init_features * 4)
+        self.trans_up2 = TransitionUp2D(init_features * 4 + n_dense_layers * growth_rate, init_features * 2)
+        self.reduce_conv2 = nn.Conv2d(init_features * 2 + init_features * 2 + n_dense_layers * growth_rate, init_features * 2, kernel_size=1)
+        self.dense_block5 = DenseBlock2D(init_features * 2, growth_rate, n_dense_layers)
 
-        self.dense_block3 = DenseBlock3D(init_features * 4, growth_rate, n_dense_layers)
-        self.trans_down3 = TransitionDown3D(init_features * 4 + n_dense_layers * growth_rate, init_features * 8)
+        self.trans_up1 = TransitionUp2D(init_features * 2 + n_dense_layers * growth_rate, init_features)
+        self.reduce_conv1 = nn.Conv2d(init_features + init_features + n_dense_layers * growth_rate, init_features, kernel_size=1)
+        self.dense_block6 = DenseBlock2D(init_features, growth_rate, n_dense_layers)
 
-        # Bottleneck
-        self.bottleneck = DenseBlock3D(init_features * 8, growth_rate, n_dense_layers)
-
-        # Decoder
-        self.trans_up3 = TransitionUp3D(init_features * 8 + n_dense_layers * growth_rate, init_features * 4)
-        self.reduce_conv3 = nn.Conv3d(init_features * 4 + init_features * 4 + n_dense_layers * growth_rate, init_features * 4, kernel_size=1)
-        self.dense_block4 = DenseBlock3D(init_features * 4, growth_rate, n_dense_layers)
-
-        self.trans_up2 = TransitionUp3D(init_features * 4 + n_dense_layers * growth_rate, init_features * 2)
-        self.reduce_conv2 = nn.Conv3d(init_features * 2 + init_features * 2 + n_dense_layers * growth_rate, init_features * 2, kernel_size=1)
-        self.dense_block5 = DenseBlock3D(init_features * 2, growth_rate, n_dense_layers)
-
-        self.trans_up1 = TransitionUp3D(init_features * 2 + n_dense_layers * growth_rate, init_features)
-        self.reduce_conv1 = nn.Conv3d(init_features + init_features + n_dense_layers * growth_rate, init_features, kernel_size=1)
-        self.dense_block6 = DenseBlock3D(init_features, growth_rate, n_dense_layers)
-
-        self.final_conv = nn.Conv3d(init_features + n_dense_layers * growth_rate, out_channels, kernel_size=1)
+        self.final_conv = nn.Conv2d(init_features + n_dense_layers * growth_rate, out_channels, kernel_size=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        # Encoder
         x1 = self.init_conv(x)
         x1 = self.dense_block1(x1)
         x2 = self.trans_down1(x1)
@@ -98,10 +89,8 @@ class Net(nn.Module):
         x3 = self.dense_block3(x3)
         x4 = self.trans_down3(x3)
 
-        # Bottleneck
         x4 = self.bottleneck(x4)
 
-        # Decoder
         x4 = self.trans_up3(x4)
         x4 = torch.cat((x4, x3), dim=1)
         x4 = self.reduce_conv3(x4)
@@ -119,4 +108,3 @@ class Net(nn.Module):
 
         x_out = self.final_conv(x2)
         return self.sigmoid(x_out)
-
